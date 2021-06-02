bb806d5b4e62f62bf4e2c656929cd280eabd70a1
Willy <wyong002@gmail.com>
2021-06-02 13:13:39 -0700

    added headless test option

--- src/test/java/test/java/FirstTest.java
+++ src/test/java/test/java/FirstTest.java
@@ -5,16 +5,17 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
 
 import utilities.ReadConfig;
 
 public class FirstTest {
 
 	ReadConfig readconfig = new ReadConfig();
+	public static WebDriver driver;
 	
 	@Test
     public void OpenBrowser()  {
-        WebDriver driver;
         System.setProperty("webdriver.chrome.driver", readconfig.getChromePath());
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
@@ -22,5 +23,6 @@ public class FirstTest {
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
-    }
+        driver.close();
+	}
 }
