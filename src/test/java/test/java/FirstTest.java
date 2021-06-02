bef7145664291f441e6c7afbc7c028b6ecd78217
Willy <wyong002@gmail.com>
2021-06-02 12:40:12 -0700

    added ReadConfig

--- src/test/java/test/java/FirstTest.java
+++ src/test/java/test/java/FirstTest.java
@@ -6,14 +6,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import utilities.ReadConfig;
+
 public class FirstTest {
 
+	ReadConfig readconfig = new ReadConfig();
+	
 	@Test
     public void OpenBrowser()  {
         WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+        System.setProperty("webdriver.chrome.driver", readconfig.getChromePath());
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
+        //options.addArguments("headless");
         driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
